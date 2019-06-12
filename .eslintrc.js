4e3ecf3f3aba3ca4be60f522a1944a906168f419
chenjian <277527478@qq.com>
2019-06-12 14:05:55 +0800

    feat : 集成babel、airbnb语法验证规则

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "rules": {
+        "no-console": "off", // 开启console
+        "semi": ["error", "never"],// 配置不开启分号;
+        "react/prefer-stateless-function": 0,// 关闭react 无状态不能使用继承
+        "react/jsx-filename-extension": 0,// 关闭JSX已.js 扩展的命名验证
+        "react/react-in-jsx-scope": 0, // 关闭js中，引入jsx验证
+        "react/prop-types": 0, // 关闭节点属性类型检测
+        "import/prefer-default-export": 0,
+        "max-len": 0,
+        "arrow-body-style": 0,
+        "react/no-array-index-key": 0,//关闭react Key不能为数据键
+        "object-curly-newline": ["error", {
+            "ObjectExpression": {
+                "multiline": true, // 如果属性内或属性之间存在换行符需要换行
+                "consistent": true // 要求无论是花括号还是两者都不直接包含换行符
+            },
+            "ObjectPattern": { "multiline": true }
+        }]
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import Home from './home'
 
 ReactDOM.render(
   <Home />,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
