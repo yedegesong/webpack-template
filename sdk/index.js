494b08577b72b51fa620aad782c1b2b0e8d47309
chenjian <277527478@qq.com>
2019-12-29 09:16:27 +0800

    feat:新增解析树

--- sdk/index.js
+++ sdk/index.js
@@ -1,2 +1,17 @@
 // 预加载更改webpack名
 export const loader = path => () => import(/* webpackChunkName: `[request]` */`pages/${path}`)
+
+// 数组树结构解析平级
+export function transformData(data, prevData = []) {
+  const childrenList = []
+  data.forEach((item) => {
+    if ((item.childs || []).length) {
+      childrenList.push(...item.childs.map(v => ({ ...v, parent: item.name })))
+    }
+    prevData.push(item)
+  })
+  if (!childrenList.length) {
+    return [...prevData]
+  }
+  return transformData(childrenList, prevData)
+}
